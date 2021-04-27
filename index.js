1207702a0373357d04d2192c37da27d49760b0ef
Matt Brown <matthewtbrown124@gmail.com>
2021-04-27 15:16:02 -0500

    commit tree de5cf3995f4d845f7bebebb45643e452fdd0f689

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ let threeUrl = "https://www.boredapi.com/api/activity?participants=3";
 let fourUrl = "https://www.boredapi.com/api/activity?participants=4";
 let fiveUrl = "https://www.boredapi.com/api/activity?participants=5";
 
+document.getElementById("box1").addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("box1").innerHTML = "Carry on below!";
+}
+
