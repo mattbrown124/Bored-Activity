85873521a860cfb8d66ec650554faafc0bd04a22
Matt Brown <matthewtbrown124@gmail.com>
2021-04-27 14:28:59 -0500

    Project 1

---
+++ index.js
@@ -0,0 +1,6 @@
+let oneUrl = "https://www.boredapi.com/api/activity?participants=1";
+let twoUrl = "https://www.boredapi.com/api/activity?participants=2";
+let threeUrl = "https://www.boredapi.com/api/activity?participants=3";
+let fourUrl = "https://www.boredapi.com/api/activity?participants=4";
+let fiveUrl = "https://www.boredapi.com/api/activity?participants=5";
+
