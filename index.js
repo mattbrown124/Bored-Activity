d979a3ee259c78aa32cb2147a6751b3c1b8987d2
Matt Brown <matthewtbrown124@gmail.com>
2021-04-29 13:46:42 -0500

    work in progress

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-let oneUrl = "https://www.boredapi.com/api/activity?participants=1";
-let twoUrl = "https://www.boredapi.com/api/activity?participants=2";
-let threeUrl = "https://www.boredapi.com/api/activity?participants=3";
-let fourUrl = "https://www.boredapi.com/api/activity?participants=4";
-let fiveUrl = "https://www.boredapi.com/api/activity?participants=5";
+const BASE_URL = "https://ghibliapi.herokuapp.com/films"
+const SEARCH_NAME_URL = BASE_URL + "/?by_name="
+const SEARCH_GEN_URL = BASE_URL + "/search?query="
+const navAll = document.getElementById("all-movies")
+const movieList = document.getElementById("search-name")
 
+const fetchAllMovies = () => {
+    // send a GET request to our BASE_URL and then we need to render each brewery to the page
+    fetch(BASE_URL)
+    .then(response => response.json())
+    .then(movies => {
+      movies.forEach(movie => {
+        console.log(movie)
+        renderMovieList(movie)
+      })
+    })
+  }
+
+  const getMovies = (e) => {
+    console.log(e.target.id)
+    fetch(BASE_URL + "/" + e.target.id)
+    .then(response => response.json())
+    .then(movie => {
+      console.log(movie)
+      renderMoviePage(movie)
+    })
+  }
+  
